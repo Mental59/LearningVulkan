dce18dc25cf4fe5837ebc7a7ba3b851aa10e00ed
Mental <Alekov99@mail.ru>
2024-11-10 21:49:39 +0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "glfw3.h"
+#include "vulkan/vulkan_core.h"
+
+static constexpr int WINDOW_WIDTH = 1280;
+static constexpr int WINDOW_HEIGHT = 720;
+
+static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vulkan", nullptr, nullptr);
+
+	if (!window)
+	{
+		std::cout << "ERROR: Failed to create window\n";
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, keyCallback);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << extensionCount << " extensions supported\n";
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+		return;
+	}
+}
